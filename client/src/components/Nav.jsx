2e642d43830ba01ed837185a09204662e387395f
Anne-Cécile Mainier <annececile.mainier@gmail.com>
2026-08-01 14:57:49 +0100

    Move logout button to Nav bar + All nav bar links for logged in users only

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,31 @@
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export function Nav() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/feed">Feed</NavLink>
-        </li>
-        <li>
-          <NavLink to="/newpost">New post</NavLink>
-        </li>
-      </ul>
+      {user && (
+        <div>
+          <ul>
+            <li>
+              <NavLink to="/feed">Feed</NavLink>
+            </li>
+            <li>
+              <NavLink to="/newpost">New post</NavLink>
+            </li>
+          </ul>
+          <p>Logged in as {user.username}</p>
+          <button onClick={handleLogout}>Log out</button>
+        </div>
+      )}
     </nav>
   );
 }
