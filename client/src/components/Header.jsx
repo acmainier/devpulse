cde4abda5ba836b48e882df98ca7f45af310d9ee
Jay-Dewhurst <jd.jsuk1@gmail.com>
2026-08-03 22:08:43 +0100

    Changed Banner location, replaced welcome text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Nav } from "./Nav";
+import devPulse from "../assets/images/devpulse2.jpg";
 import "./Header.css";
 
 export function Header() {
   return (
     <header>
       <div className="header-inner">
-        <h1 className="h1-welcome">Welcome to DevPulse</h1>
+        <img src={devPulse} alt="DevPulse logo header image" className="welcome-banner" />
         <Nav />
       </div>
     </header>
